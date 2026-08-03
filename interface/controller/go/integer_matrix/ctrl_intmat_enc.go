e19e24a5b3d076d542c86d6d94873fcb38a3465e
Mingyu Jeong <159212138+RFA0608@users.noreply.github.com>
2026-08-03 11:08:17 +0900

    Update ctrl_intmat_enc.go

--- interface/controller/go/integer_matrix/ctrl_intmat_enc.go
+++ interface/controller/go/integer_matrix/ctrl_intmat_enc.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"time"
 
-	tccp "github.com/RFA0608/QQS3C/communication/go/tpc"
+	tccp "github.com/CDSL-EncryptedControl/QQS3C/communication/go/tpc"
 
 	utils "github.com/CDSL-EncryptedControl/CDSL/utils"
 	RLWE "github.com/CDSL-EncryptedControl/CDSL/utils/core/RLWE"
