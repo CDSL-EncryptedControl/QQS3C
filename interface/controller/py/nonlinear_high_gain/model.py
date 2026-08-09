4bdf62b0e160d66b75a66b2c348f4b84f1829174
Mingyu Jeong <159212138+RFA0608@users.noreply.github.com>
2026-08-10 08:00:22 +0900

    Update model.py

--- interface/controller/py/nonlinear_high_gain/model.py
+++ interface/controller/py/nonlinear_high_gain/model.py
@@ -3,10 +3,10 @@ import control as ct
 
 class nobs:
     # hyperparams
-    ts = 0.02
-    alpha = np.array([[1.5, 0.4]], dtype=float)
-    beta = np.array([[1.5, 0.4]], dtype=float)
-    contractive_ratio = 0.6
+    ts = 0.01
+    alpha = np.array([[2, 1]], dtype=float)
+    beta = np.array([[2, 1]], dtype=float)
+    contractive_ratio = 0.72
     epsilon = 1
 
     Km = 0.042
